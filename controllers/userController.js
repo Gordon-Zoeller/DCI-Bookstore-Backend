85f03b6fb4e34efa39cc5996c65487d81eba51e3
pozniej-znajde-wolne-haslo <aniarafeld@o2.pl>
2023-11-30 00:02:01 +0100

    changes: 'res.json()' in catch-blocks into 'next(err)' AND adjusts: the json-responses

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,80 +1,88 @@
 import UserModel from '../models/userSchema.js';
 
 // Function to get all users
-export const getAllUsers = async (req, res) => {
+export const getAllUsers = async (req, res, next) => {
   try {
     const users = await UserModel.find();
-    res.status(200).json(users);
+    res.status(200).json({ success: true, data: users });
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal Server Error' });
+    next(error);
   }
 };
 
 // Function to get a specific user by ID
-export const getUserById = async (req, res) => {
+export const getUserById = async (req, res, next) => {
   const userId = req.params.id;
 
-  try {// import { createUser, getAllUsers, getUserById } from "./controllers/userController.js";
+  try {
+    // import { createUser, getAllUsers, getUserById } from "./controllers/userController.js";
     const user = await UserModel.findById(userId);
-    
+
     if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+      return res
+        .status(404)
+        .json({ success: false, message: 'User not found' });
     }
 
-    res.status(200).json(user);
+    res.status(200).json({ success: true, data: user });
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal Server Error' });
+    next(error);
   }
 };
 
 // Function to create a new user
-export const createUser = async (req, res) => {
+export const createUser = async (req, res, next) => {
   const userData = req.body;
 
   try {
     const newUser = await UserModel.create(userData);
-    res.status(201).json(newUser);
+    res.status(201).json({ success: true, data: newUser });
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal Server Error' });
+    next(error);
   }
 };
 
 // Function to update a user by ID
-export const updateUserById = async (req, res) => {
+export const updateUserById = async (req, res, next) => {
   const userId = req.params.id;
   const updatedUserData = req.body;
 
   try {
-    const updatedUser = await UserModel.findByIdAndUpdate(userId, updatedUserData, { new: true });
-    
+    const updatedUser = await UserModel.findByIdAndUpdate(
+      userId,
+      updatedUserData,
+      { new: true }
+    );
+
     if (!updatedUser) {
-      return res.status(404).json({ message: 'User not found' });
+      return res
+        .status(404)
+        .json({ success: false, message: 'User not found' });
     }
 
-    res.status(200).json(updatedUser);
+    res.status(200).json({ success: true, data: updatedUser });
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal Server Error' });
+    next(error);
   }
 };
 
 // Function to delete a user by ID
-export const deleteUserById = async (req, res) => {
+export const deleteUserById = async (req, res, next) => {
   const userId = req.params.id;
 
   try {
     const deletedUser = await UserModel.findByIdAndDelete(userId);
-    
+
     if (!deletedUser) {
-      return res.status(404).json({ message: 'User not found' });
+      return res
+        .status(404)
+        .json({ success: false, message: 'User not found' });
     }
 
-    res.status(200).json({ message: 'User deleted successfully' });
+    res
+      .status(200)
+      .json({ success: true, message: 'User deleted successfully' });
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal Server Error' });
+    next(error);
   }
 };
