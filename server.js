29ad0f0b480c02b8b3c352952d45b02bb98d028a
Sam <sam234@yahoo.com>
2023-11-29 15:16:15 +0100

    create: orderController, orderRoutes

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import morgan from "morgan";
 import { mongooseConnection } from "./connection/mongoose.js";
 import { errorStatus, pageNotFound } from "./middleware/errors.js";
+import OrderRoutes from "./routes/orderRoutes.js";
+import BookModel from "./models/bookSchema.js";
 
 const app = express();
 
@@ -10,16 +13,21 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors({
-    origin:`${process.env.FRONTEND}`,
+app.use(
+  cors({
+    origin: `${process.env.FRONTEND}`,
     // methods: 'GET,POST,PATCH,PUT,DELETE'
-}))
+  })
+);
+
+app.use(morgan("tiny"));
 
 mongooseConnection();
 
-// your code here 
+// your code here
+app.use("/api/order", OrderRoutes);
 
 app.use(pageNotFound);
 app.use(errorStatus);
 
-app.listen(PORT, () => console.log('server running on port', PORT));
+app.listen(PORT, () => console.log("server running on port", PORT));
