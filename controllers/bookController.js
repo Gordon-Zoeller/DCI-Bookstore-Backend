7744716760999f9348d8308ec76c4c19e74548ab
pozniej-znajde-wolne-haslo <aniarafeld@o2.pl>
2023-12-06 14:34:41 +0100

    adds IMAGE Schema, controllers, and a new BOOK controller

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -36,28 +36,31 @@ const createBook = async (req, res, next) => {
 
 const updateBook = async (req, res, next) => {
   try {
-    // for the IMG-upload (when using FORM-data)
-    // if there's an IMG in the REQ, add it to the IMAGES-collection && 'thumbnail' to the BOOK-document:
+    const book = await BookModel.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.json({ success: true, data: book });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const uploadImageToBook = async (req, res, next) => {
+  // using FORM-data
+  // if there's an IMG in the REQ, add it to the IMAGES-collection && 'thumbnail' to the BOOK-document:
+  try {
     const book = await BookModel.findById(req.params.id);
     if (req?.files.image) {
       const image = await ImageModel.create({
         filename: Date.now() + '' + req.files.image.name,
         data: req.files.image.data,
-        userId: req.params.id,
+        bookId: req.params.id,
       });
 
       book.thumbnail = `http://localhost:${process.env.PORT}/api/images/${image.filename}`;
       await book.save();
+      res.json({ success: true, message: 'image uploaded successfully' });
     }
-
-    const updatedBook = await BookModel.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      {
-        new: true,
-      }
-    );
-    res.json({ success: true, data: updatedBook });
   } catch (error) {
     next(error);
   }
--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -5,6 +5,7 @@ import {
   getBooks,
   searchBook,
   updateBook,
+  uploadImageToBook,
 } from '../controllers/bookController.js';
 import { authorization } from '../middleware/authorisation.js';
 import { role } from '../middleware/role.js';
@@ -15,6 +16,8 @@ route.get('/', getBooks);
 route.get('/search/:regex', searchBook);
 route.post('/create', authorization, role, createBook);
 route.patch('/update/:id', authorization, role, updateBook);
+// POST image - only on the backend side, --> AUTH & ROLE inactive
+route.post('/upload-img/:id', /* authorization, role, */ uploadImageToBook);
 route.delete('/delete/:id', authorization, role, deleteBook);
 
 export default route;
